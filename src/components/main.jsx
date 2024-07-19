3d63113592b96c57f9fc2196999a195fb0e69b01
luchosaavedra <luchomontana93@gmail.com>
2024-07-19 16:11:41 -0400

    cuarto commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function Main() {
-    
+function Main({nota, EliminarNota}) {
+    // Desestructuramos las propiedades de la nota
+    const { title, description, important } = nota;
+    console.log(nota);
+    return (
+        <main>
+            {/* Si la nota es importante le pasamos la clase important */}
+            <div className={important ? 'note important' : 'note'}>
+                <div className='flex'>
+                    <h2>{title}</h2>
+                    <button onClick={() => EliminarNota(title)}>X</button>
+                </div>
+                <p>{description}</p>
+            </div>
+        </main>
+    )
 }
 
 export default Main;
